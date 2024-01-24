7f8f939ba82397f9d312c05d9200853524e1a8f1
hrustinszkiadam <hrustinszkiadam@gmail.com>
2024-01-24 11:11:10 +0100

    remap for end of line to "ű"

--- lua/hrustinszkiadam/remap.lua
+++ lua/hrustinszkiadam/remap.lua
@@ -1,4 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set('n', '!', '$', { noremap = true, silent = true })
+vim.keymap.set('n', 'ű', '$', { noremap = true, silent = true })
 vim.keymap.set('n', 'é', ':', { noremap = true})
