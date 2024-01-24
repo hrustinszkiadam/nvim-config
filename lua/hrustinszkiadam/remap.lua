f030eff0ff3f84384c75edb001dbb30b9fe1f7e2
hrustinszkiadam <hrustinszkiadam@gmail.com>
2024-01-24 11:49:48 +0100

    remap single line move

--- lua/hrustinszkiadam/remap.lua
+++ lua/hrustinszkiadam/remap.lua
@@ -7,6 +7,9 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==")
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==")
+
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
@@ -14,4 +17,5 @@ vim.keymap.set('n', 'ű', '$', { noremap = true, silent = true })
 vim.keymap.set('n', 'é', ':', { noremap = true})
 
 --Source
+vim.keymap.set("n", "<leader>w", vim.cmd.w) 
 vim.keymap.set("n", "<leader><leader>", vim.cmd.so) 
