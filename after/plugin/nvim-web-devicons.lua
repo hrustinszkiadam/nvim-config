7c449cba056d41eb328ccfbf5bd7d1b9e5b5131e
hrustinszkiadam <hrustinszkiadam@gmail.com>
2024-01-26 13:51:52 +0100

    web-devicons

---
+++ after/plugin/nvim-web-devicons.lua
@@ -0,0 +1,7 @@
+require('nvim-web-devicons').setup {
+    color_icons = true,
+    default = true,
+    strict = true
+}
+
+require'nvim-web-devicons'.get_icons()
--- lua/hrustinszkiadam/packer.lua
+++ lua/hrustinszkiadam/packer.lua
@@ -2,12 +2,13 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
   use({ 'rose-pine/neovim', as = 'rose-pine', config = function() vim.cmd('colorscheme rose-pine') end })
   use({ 'folke/tokyonight.nvim', as = 'tokyonight', config = function() vim.cmd('colorscheme tokyonight') end })
+  use 'nvim-tree/nvim-web-devicons'
+  use {
+      'nvim-telescope/telescope.nvim', tag = '0.1.5',
+      requires = { {'nvim-lua/plenary.nvim'} }
+  }
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use("nvim-treesitter/nvim-treesitter-context")
   use('nvim-treesitter/playground')
