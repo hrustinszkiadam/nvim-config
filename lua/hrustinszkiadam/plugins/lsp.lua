5319f28d337c585fab3ead69aa15ebdd63e6fa43
hrustinszkiadam <hrustinszkiadam@gmail.com>
2024-01-29 16:26:15 +0100

    cmp bindings

--- lua/hrustinszkiadam/plugins/lsp.lua
+++ lua/hrustinszkiadam/plugins/lsp.lua
@@ -5,7 +5,6 @@ return {
         lazy = true,
         config = false,
         init = function()
-            -- Disable automatic setup, we are doing it manually
             vim.g.lsp_zero_extend_cmp = 0
             vim.g.lsp_zero_extend_lspconfig = 0
         end,
@@ -24,23 +23,27 @@ return {
             {'L3MON4D3/LuaSnip'},
         },
         config = function()
-            -- Here is where you configure the autocompletion settings.
             local lsp_zero = require('lsp-zero')
             lsp_zero.extend_cmp()
 
-            -- And you can configure cmp even more, if you want to.
             local cmp = require('cmp')
-            local cmp_action = lsp_zero.cmp_action()
+            local cmp_select = {behavior = cmp.SelectBehavior.Select}
 
             cmp.setup({
+                sources = {
+                    {name = 'path'},
+                    {name = 'nvim_lsp'},
+                    {name = 'nvim_lua'},
+                    {name = 'luasnip', keyword_length = 2},
+                    {name = 'buffer', keyword_length = 3},
+                },
                 formatting = lsp_zero.cmp_format(),
                 mapping = cmp.mapping.preset.insert({
+                    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
                     ['<C-Space>'] = cmp.mapping.complete(),
-                    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-                    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-                    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-                    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-                })
+                }),
             })
         end
     },
@@ -55,15 +58,11 @@ return {
             {'williamboman/mason-lspconfig.nvim'},
         },
         config = function()
-            -- This is where all the LSP shenanigans will live
             local lsp_zero = require('lsp-zero')
             lsp_zero.extend_lspconfig()
 
-            --- if you want to know more about lsp-zero and mason.nvim
-            --- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
             lsp_zero.on_attach(function(client, bufnr)
-                -- see :help lsp-zero-keybindings
-                -- to learn the available actions
+                -- lsp keybindings come here
                 lsp_zero.default_keymaps({buffer = bufnr})
             end)
 
@@ -72,7 +71,6 @@ return {
                 handlers = {
                     lsp_zero.default_setup,
                     lua_ls = function()
-                        -- (Optional) Configure lua language server for neovim
                         local lua_opts = lsp_zero.nvim_lua_ls()
                         require('lspconfig').lua_ls.setup(lua_opts)
                     end,
