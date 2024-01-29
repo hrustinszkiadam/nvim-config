3d9be11fd310e88c2e6b99423e44747beb49d706
hrustinszkiadam <hrustinszkiadam@gmail.com>
2024-01-29 16:33:15 +0100

    <C-z> now confirms lsp suggestion

--- lua/hrustinszkiadam/plugins/lsp.lua
+++ lua/hrustinszkiadam/plugins/lsp.lua
@@ -41,7 +41,7 @@ return {
                 mapping = cmp.mapping.preset.insert({
                     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                    ['<C-z>'] = cmp.mapping.confirm({ select = true }),
                     ['<C-Space>'] = cmp.mapping.complete(),
                 }),
             })
