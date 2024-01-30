5961f58e67a9eaee9352e358d5f11d2e54a5b0f8
hrustinszkiadam <hrustinszkiadam@gmail.com>
2024-01-30 09:25:31 +0100

    added vscode modern theme

--- lua/hrustinszkiadam/plugins/colorscheme.lua
+++ lua/hrustinszkiadam/plugins/colorscheme.lua
@@ -32,4 +32,14 @@ return {
             })
             end,
     },
+    {
+        "gmr458/vscode_modern_theme.nvim",
+        config = function()
+            require("vscode_modern").setup({
+                cursorline = true,
+                transparent_background = true,
+                nvim_tree_darker = true,
+            })
+        end,
+    },
 }
