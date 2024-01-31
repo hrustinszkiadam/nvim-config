dd0ac40b017868de29aabf6f3607bd8183e6373e
hrustinszkiadam <hrustinszkiadam@gmail.com>
2024-01-31 11:35:54 +0100

    Telescope version 0.1.x

--- lua/hrustinszkiadam/plugins/telescope.lua
+++ lua/hrustinszkiadam/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
     {
         'nvim-telescope/telescope.nvim',
-        version = '0.1.5',
+        version = '0.1.x',
         dependencies = {
             'nvim-lua/plenary.nvim'
         },
