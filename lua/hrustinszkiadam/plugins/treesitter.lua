9945ff6ee29367c61ca245694e4021441582acec
hrustinszkiadam <hrustinszkiadam@gmail.com>
2024-02-02 16:04:27 +0100

    can now exit terminal mode with <ESC>

--- lua/hrustinszkiadam/plugins/treesitter.lua
+++ lua/hrustinszkiadam/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
         config = function()
             require('nvim-treesitter.configs').setup({
                 ensure_installed = { "lua", "javascript", "typescript", "c", "vim", "vimdoc", "query" },
-                sync_install = false,
+                sync_install = true,
                 auto_install = true,
                 highlight = {
                     enable = true,
