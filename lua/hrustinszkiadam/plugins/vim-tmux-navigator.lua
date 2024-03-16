e9f65d7844ad22be7012cbe256bba008204f7e23
hrustinszkiadam <hrustinszkiadam@gmail.com>
2024-03-16 14:57:58 +0100

    feat: vim-tmux-navigator for linux

---
+++ lua/hrustinszkiadam/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        'christoomey/vim-tmux-navigator',
+        lazy = false,
+        config = function()
+            vim.keymap.set('n', '<C-h>', '<cmd>TmuxNavigateLeft<CR>')
+            vim.keymap.set('n', '<C-j>', '<cmd>TmuxNavigateDown<CR>')
+            vim.keymap.set('n', '<C-k>', '<cmd>TmuxNavigateUp<CR>')
+            vim.keymap.set('n', '<C-l>', '<cmd>TmuxNavigateRight<CR>')
+        end,
+    }
+}
