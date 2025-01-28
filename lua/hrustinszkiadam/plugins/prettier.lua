e154d76db5646dd5fc054b40c1dab615b5027789
hrustinszkiadam <hrustinszkiadam@gmail.com>
2025-01-28 21:10:14 +0100

    fix: Prettier

--- lua/hrustinszkiadam/plugins/prettier.lua
+++ lua/hrustinszkiadam/plugins/prettier.lua
@@ -1,18 +1,60 @@
 return {
-    'MunifTanjim/prettier.nvim',
-    dependencies = {
-        'neovim/nvim-lspconfig',
-        'jose-elias-alvarez/null-ls.nvim'
+    {
+        'jose-elias-alvarez/null-ls.nvim',
+        config = function()
+            local null_ls = require("null-ls")
+
+            local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+            local event = "BufWritePre" -- or "BufWritePost"
+            local async = event == "BufWritePost"
+
+            null_ls.setup({
+                on_attach = function(client, bufnr)
+                    if client.supports_method("textDocument/formatting") then
+                        vim.keymap.set("n", "<leader>f", function()
+                            vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+                        end, { buffer = bufnr, desc = "[lsp] format" })
+
+                        -- format on save
+                        vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+                        vim.api.nvim_create_autocmd(event, {
+                            buffer = bufnr,
+                            group = group,
+                            callback = function()
+                                vim.lsp.buf.format({ bufnr = bufnr, async = async })
+                            end,
+                            desc = "[lsp] format on save",
+                        })
+                    end
+
+                    if client.supports_method("textDocument/rangeFormatting") then
+                        vim.keymap.set("x", "<leader>f", function()
+                            vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+                        end, { buffer = bufnr, desc = "[lsp] format" })
+                    end
+                end,
+            })
+        end,
     },
-    config = function()
-        require('prettier').setup({
-            tab_width = 4,
-            use_tabs = true,
-            semi = true,
-            single_quote = true,
-            jsx_single_quote = true,
-            single_attribute_per_line = true,
-        })
-        vim.keymap.set('n', '<leader>ü', vim.cmd.Prettier)
-    end,
+    {
+        'MunifTanjim/prettier.nvim',
+        dependencies = {
+            'neovim/nvim-lspconfig',
+            'jose-elias-alvarez/null-ls.nvim'
+        },
+        config = function()
+            require('prettier').setup({
+                cli_options = {
+                    tab_width = 4,
+                    use_tabs = true,
+                    semi = true,
+                    single_quote = true,
+                    jsx_single_quote = true,
+                    single_attribute_per_line = true,
+                    config_precedence = "cli-override",
+                },
+            })
+            vim.keymap.set('n', '<leader>ü', vim.cmd.Prettier)
+        end,
+    }
 }
